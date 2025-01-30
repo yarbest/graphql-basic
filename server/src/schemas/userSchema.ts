dc3b4fdd5b71c89342a027ed9ae9f2077adb2482
yarbest <yarbest15@gmail.com>
2025-01-30 17:05:44 +0200

    [server] add user schema

---
+++ server/src/schemas/userSchema.ts
@@ -0,0 +1,39 @@
+import { buildSchema } from 'graphql'
+
+export const schema = buildSchema(/* GraphQL */ `
+  type User {
+    id: ID!
+    username: String!
+    age: Int
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String
+  }
+
+  input UserInput {
+    id: ID
+    username: String!
+    age: Int!
+    posts: [PostInput]    
+  }
+
+  input PostInput {
+    id: ID
+    title: String!
+    content: String!
+  }
+
+  type Query {
+    getAllUsers: [User]
+    getUser(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(input: UserInput!): User
+    deleteUser(id: ID!): User
+  }
+`)
