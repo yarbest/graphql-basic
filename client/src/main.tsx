cb05c3fd8bde597db9e5778c24cb824a7984fa5a
yarbest <yarbest15@gmail.com>
2025-01-30 17:10:31 +0200

    [client] add apollo to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,16 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.tsx'
 
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+})
+
 createRoot(document.getElementById('root')!).render(
-  <App />,
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>,
 )
